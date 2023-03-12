3f8a13daec46463031430437d0f7f800ea13ebd3
jules <jules.rau@gmx.de>
2023-03-12 18:59:43 +0100

    add some fancystyling

--- classes/MyGUI.py
+++ classes/MyGUI.py
@@ -19,28 +19,76 @@ class MyGUI:
     def create_file_dropdown(self):
         # Create a label for the dropdown
         label = tk.Label(
-            master = self.master, 
-            text   = "Select a file:"
+            master = self.root, 
+            text   = "Select a file:",
+            font   = ("Arial", 14),
+            fg     = "#303030",
+            bg     = "#f2f2f2",
+            padx   = 10,
+            pady   = 10,
         )
-        label.pack()
+        label.pack(
+            side   = tk.TOP, 
+            anchor = tk.NW
+        )
 
         # Get a list of all XML and JSON files in the "logs/uilogs/" directory
         file_list = [f for f in os.listdir(path_to_files + "/" + log_dir + "/") if f.endswith(".xml") or f.endswith(".csv")]
 
         # Create a dropdown with the file names
-        dropdown = ttk.Combobox(
-            master = self.master, 
-            values = file_list
+        dropdown_frame = tk.Frame(self.root, bg = "#f2f2f2")
+        dropdown_frame.pack(
+            padx = 10, 
+            pady = 10, 
+            fill = tk.X
         )
-        dropdown.pack()
+
+        tk.Label(
+            master = dropdown_frame, 
+            text   = "Select file:",
+            font   = ("Arial", 12),
+            fg     = "#303030",
+            bg     = "#f2f2f2",
+            padx   = 10,
+            pady   = 10,
+        ).pack(side = tk.LEFT)
+
+        dropdown = ttk.Combobox(
+            master = dropdown_frame, 
+            values = file_list,
+            font   = ("Arial", 12),
+            width  = 30,
+        )
+        dropdown.pack(
+            side = tk.LEFT, 
+            padx = 10
+        )
 
         # Create a "Load" button that doesn't perform any action
-        button = tk.Button(
-            master  = self.master, 
-            text    = "Load", 
-            command = lambda: self.create_widgets(dropdown.get())
+        button_frame = tk.Frame(self.root, bg = "#f2f2f2")
+        button_frame.pack(
+            padx = 10, 
+            pady = 10, 
+            fill = tk.X
         )
-        button.pack()
+
+        tk.Button(
+            master  = button_frame, 
+            text    = "Load", 
+            font    = ("Arial", 12),
+            bg      = "#303030",
+            fg      = "#f2f2f2",
+            padx    = 10,
+            pady    = 10,
+            command = lambda: self.create_widgets(dropdown.get())
+        ).pack(side = tk.RIGHT)
+
+        # Add some padding at the bottom of the window
+        tk.Frame(
+            self.root, 
+            height = 20, 
+            bg     = "#f2f2f2"
+        ).pack(fill = tk.X)
         
     def clear_window(self):
         # Destroy all child widgets of the window
@@ -67,13 +115,17 @@ class MyGUI:
                 if isinstance(widget, tk.Label) and widget.cget("text").startswith("Error:"):
                     widget.destroy()
 
+            if len(extension) > 0:
+                errorText = f"Error: Unsupported file type '{extension}'"
+            else:
+                errorText = "Error: You need to select a file"
+
             # Unsupported file type
-            label = tk.Label(
+            tk.Label(
                 master     = self.master, 
-                text       = f"Error: Unsupported file type '{extension}'",
+                text       = errorText,
                 foreground = "#FF0000"
-            )
-            label.pack()
+            ).pack()
             return
         
         if "pomp_dim" not in self.arr.columns:
