983343e8d6fe1812ca8b61b2d5f9db072ff36421
thohenadl <110967363+thohenadl@users.noreply.github.com>
2023-06-27 10:35:29 +0200

    Updated Concat Path, Added Manual

--- concat.py
+++ concat.py
@@ -3,6 +3,6 @@ from util.csvUtil import *
 
 if __name__ == "__main__":
     # Edit the path to navigate to the CSV files
-    file_path = "C:/Users/tomho/OneDrive/Documents/Promotion/smartRPA-action_logger/logs/ICPM Dataset"
+    file_path = "logs/uilogs"
     data = read_multiple_csv_files(file_path)
     store_log(data, file_path, "concatenated_file.csv", ",")
